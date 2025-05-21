753e1eac1850516c232ccf4f438956b9edc3ee83
Jakub <sulisz.jakub@gmail.com>
2025-05-21 10:33:15 +0200

    next init

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -7,11 +8,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//* Hiding API KEY
+dotenv.config();
+
 //* Routes
 app.get("/", (req, res) => {
   res.json("Simple Animal App");
 });
 
+app.get("/cat", (req, res) => {
+  async function fetchCats() {
+    try {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${process.env.KEY}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        res.status(200).json(data);
+      } else {
+        throw new Error("Could not fetch");
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  fetchCats();
+});
 
 //* Global Error Handler
 app.use((err, req, res, next) => {
@@ -21,8 +45,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-const port = 3003;
+const port = process.env.PORT || 3003;
 
 app.listen(port, () => {
   console.log(`server is listening @ ${port}`);
